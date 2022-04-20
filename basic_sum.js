930543e7257ea8bc76ec859b35a09b5662356ec0
debbyblessing <debbyblessing1234@gmail.com>
2022-04-20 18:47:06 +0100

    adding property

--- basic_sum.js
+++ basic_sum.js
@@ -1,9 +1,9 @@
 /*summition of numbers */
-function  sum(a, b){
-    let sum = 0;
-   for (let i = 1; i <= sum; i++){
+function sum(a, b) {
+  let sum = 0;
+  for (let i = 1; i <= sum; i++) {
     sum += i;
-   }
-  return  a + b;
   }
-  console.log(sum(3, 2)); 
+  return a + b;
+}
+console.log(sum(3, 2));
