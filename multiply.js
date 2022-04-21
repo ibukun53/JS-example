6c43ee15a9cac93a3ca51d90a5fe30652c14945e
debbyblessing <debbyblessing1234@gmail.com>
2022-04-21 22:50:09 +0100

    fix prop

--- multiply.js
+++ multiply.js
@@ -1,13 +1,13 @@
 /*multiplication of numbers */
 function multiply(a, b){
-   if( a *= b) {
-       return multiply; 
+   if( a == b) {
+       return a*b; 
    }else{
        return null ;
    }
 
 }
 
-console.log(4 * 3);
+console.log(multiply(4,4));
 
 
