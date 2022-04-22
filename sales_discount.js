ab137220a463e5fa2b478a9a44adbf500c5c5269
debbyblessing <debbyblessing1234@gmail.com>
2022-04-22 21:49:19 +0100

    add content

---
+++ sales_discount.js
@@ -0,0 +1,19 @@
+  
+      /* function price(actualAmount){
+           const actualAmount=amount ;
+           let i= '';
+        if( i >=5000== 0.02 , i>=80000==0.03, i>=400000==0.04,  i>=3000000==0.05) {
+       return  i *(100- discount/100)
+           };
+    };
+    
+           console.log (actualAmount(200, 2));
+           */
+
+           function actualAmount(amo, dis){
+                  if (0< 5000){ 
+                     return(amo/100)*dis;
+                  }
+           };
+           console.log(actualAmount(6000,0.2))
+        
