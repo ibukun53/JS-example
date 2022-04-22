b213ec7d7b44e5664f43dee207ad4bf275a1dac9
debbyblessing <debbyblessing1234@gmail.com>
2022-04-22 23:07:11 +0100

    refixing

--- basic_sum.js
+++ basic_sum.js
@@ -1,11 +1,7 @@
-/*summition of numbers */
+
 function add(n,m) {
-  if (n && m) {
+  if (n, m) {
     return (n + m);
-  } else
-  {
-    return  1 + (n +m);
-  }
-  
+  } 
 }
-console.log(add(7,2));
+console.log(add(12,80));
