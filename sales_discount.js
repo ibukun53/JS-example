32d20c1d5a4ca4674812a5f6c93684fe394647f7
debbyblessing <debbyblessing1234@gmail.com>
2022-04-22 22:27:56 +0100

    fixing content

--- sales_discount.js
+++ sales_discount.js
@@ -1,19 +1,7 @@
-  
-      /* function price(actualAmount){
-           const actualAmount=amount ;
-           let i= '';
-        if( i >=5000== 0.02 , i>=80000==0.03, i>=400000==0.04,  i>=3000000==0.05) {
-       return  i *(100- discount/100)
-           };
-    };
-    
-           console.log (actualAmount(200, 2));
-           */
-
-           function actualAmount(amo, dis){
-                  if (0< 5000){ 
-                     return(amo/100)*dis;
+function actualAmount(amo, dis){
+                  if (0< 5000*0.02 && 0<80000*0.03 && 0<400000*0.04 && 0<3000000*0.05){ 
+                     return amo*(100-dis/100);
                   }
            };
-           console.log(actualAmount(6000,0.2))
+           console.log(actualAmount(3500000,0.05));
         
