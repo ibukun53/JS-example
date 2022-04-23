66fe21e10be05ab2d260fca17fe7f912f8538886
debbyblessing <debbyblessing1234@gmail.com>
2022-04-23 18:09:48 +0100

    refixing code

--- divide.js
+++ divide.js
@@ -1,7 +1,5 @@
-const divide = (a, b)=>{ 
-    return a /= b; 
-};
+const divide = (a, b) => a / b;
 
-console.log(divide(6,2));
-console.log(divide(30,5));
-console.log(divide(45,9));
+divide(6, 2);
+divide(30, 5);
+divide(45, 9);
