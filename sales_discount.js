eeb43aa25054d8eef6cfac4f51b381879afc0178
debbyblessing <debbyblessing1234@gmail.com>
2022-04-23 12:43:01 +0100

    fixing bug

--- sales_discount.js
+++ sales_discount.js
@@ -1,5 +1,5 @@
-function actualAmount(amo, dis){
-                  if (0< 5000*0.02 && 0<80000*0.03 && 0<400000*0.04 && 0<3000000*0.05){ 
+const actualAmount= (amo, dis) => {
+               const paid= 0< 5000*0.02 && 0<80000*0.03 && 0<400000*0.04 && 0<3000000*0.05;{ 
                      return amo*(100-dis/100);
                   }
            };
