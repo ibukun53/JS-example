15b0637bc237231b4803c757ecf5b1b5443ebf91
debbyblessing <debbyblessing1234@gmail.com>
2022-04-23 18:11:56 +0100

    refixing code

--- sales_discount.js
+++ sales_discount.js
@@ -1,7 +1,4 @@
-const actualAmount= (amo, dis) => {
-               const paid= 0< 5000*0.02 && 0<80000*0.03 && 0<400000*0.04 && 0<3000000*0.05;{ 
-                     return amo*(100-dis/100);
-                  }
-           };
-           console.log(actualAmount(3500000,0.05));
-        
+const amountTTag = (5000 * 0.02 < 0 && 80000 * 0.03 < 0 && 400000 * 0.04 < 0 && 3000000 * 0.05 < 0);
+const actualAmount = (amo, dis) => amo * (100 - dis / 100);
+
+actualAmount(3500000, 0.05);
