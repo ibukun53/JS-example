0bdc26c1619b50012ce21e95c9cf39a0cf5aeb98
debbyblessing <debbyblessing1234@gmail.com>
2022-04-24 16:25:09 +0100

    fixing code

--- sales_discount.js
+++ sales_discount.js
@@ -1,5 +1,3 @@
-const amountTTag = (5000 * 0.02 < 0 && 80000 * 0.03 < 0 && 400000 * 0.04 < 0 && 3000000 * 0.05 < 0);
 const actualAmount = (amo, dis) => amo * (100 - dis / 100);
 
 actualAmount(3500000, 0.05);
-amountTTag();
