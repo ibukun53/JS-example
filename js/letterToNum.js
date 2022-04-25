786870d083ad5f8e888a0db2bfa6e7d0393e6d61
debbyblessing <debbyblessing1234@gmail.com>
2022-04-25 10:50:40 +0100

    changed folder name

---
+++ js/letterToNum.js
@@ -0,0 +1,11 @@
+const numToLetter = (str) => {
+  const orderedNums = str
+    .toLowerCase()
+    .split('')
+    .map((value) => value.charCodeAt() - 96)
+    .join('');
+  return orderedNums;
+};
+
+numToLetter('b');
+numToLetter('zep');
