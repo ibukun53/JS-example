bd34cb1fbd44ea14a8525b52b5ed031a97fc80c0
debbyblessing <debbyblessing1234@gmail.com>
2022-04-28 11:46:07 +0100

    implementing error

--- js/turnToEvenNumber.js
+++ js/turnToEvenNumber.js
@@ -1,11 +1,15 @@
-function turnTOEvenNumber(num) {
-    if (num % 2 != 0) {
-        return (num * 2);
-
-    } else (num % 2 == 0) {
-        return (num * 2);
+// eslint-disable-next-line consistent-return
+const turnToEvenNumber = (num) => {
+  const oddnum = (num % 2 !== 0);
+  {
+    if (oddnum === true) {
+      return (num * 2);
     }
-
-}
-turnTOEvenNumber(17);
-turnTOEvenNumber(8);
+    const evennum = (num % 2 === 0);
+    if (evennum === true) {
+      return (num);
+    }
+  }
+};
+turnToEvenNumber(10);
+turnToEvenNumber(7);
