f8cc2d0583717f13b5c284e154360a82b9ab9b9b
debbyblessing <debbyblessing1234@gmail.com>
2022-04-28 12:35:43 +0100

    fixing error

--- js/turnToEvenNumber.js
+++ js/turnToEvenNumber.js
@@ -1,15 +1,13 @@
-// eslint-disable-next-line consistent-return
 const turnToEvenNumber = (num) => {
-  const oddnum = (num % 2 !== 0);
-  {
-    if (oddnum === true) {
-      return (num * 2);
-    }
-    const evennum = (num % 2 === 0);
-    if (evennum === true) {
-      return (num);
-    }
+  const evennum = (num % 2 === 0);
+  if (evennum === true) {
+    return num;
   }
+  const oddnum = (num % 2 !== 0);
+  if (oddnum === true) {
+    return num * 2;
+  }
+  return num;
 };
 turnToEvenNumber(10);
 turnToEvenNumber(7);
