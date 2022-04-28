998dba00c95597f35ef2093266b5122a98dab912
debbyblessing <debbyblessing1234@gmail.com>
2022-04-28 11:45:22 +0100

    updating new prop

--- js/sumOfArray.js
+++ js/sumOfArray.js
@@ -1,11 +1,7 @@
-function sumOfArray(array) {
-  let i;
-  let sum = 0;
-  for (i = 0; i < array.length; i + 1) {
-    sum += array.length[i];
-  }
-  return sum;
+const array = [1, 2, 3, 4];
+let sum = 0;
+
+for (let i = 0; i < array.length; i + 1) {
+  sum += array[i];
 }
-sumOfArray([2, 3, 4, 5]);
-sumOfArray([]);
-sumOfArray([0, -1, 3]);
+sum();
