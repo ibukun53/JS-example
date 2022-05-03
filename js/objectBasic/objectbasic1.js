cfba71e4a72d903fb7db91fe74105e6359168bb6
debbyblessing <debbyblessing1234@gmail.com>
2022-05-03 07:30:43 +0100

    fixing bugs

--- js/objectBasic/objectbasic1.js
+++ js/objectBasic/objectbasic1.js
@@ -10,8 +10,9 @@ const cat = {
 // Put your code here
 // eslint-disable-next-line dot-notation
 const catName = cat['name'];
-cat.greeting();
-cat.color = ('black');
+// eslint-disable-next-line no-console
+console.log(cat.greeting);
+cat.color = 'black';
 // Don't edit the code below here
 const section = document.querySelector('section');
 const para1 = document.createElement('p');
--- js/objectBasic/objectbasic2.js
+++ js/objectBasic/objectbasic2.js
@@ -1,21 +1,21 @@
 let bandInfo;
 
 // Put your code here
-// eslint-disable-next-line prefer-const
-bandInfo = {
-  name: 'miracle band',
-  nationality: 'Nigeria',
-  genre: 'R&B',
-  members: 50,
-  formed: '5yrs',
-  split: 2,
+const info = {
+  name: ['miracle band'],
+  nationality: ['Nigeria'],
+  genre: ['R&B'],
+  members: [50],
+  formed: ['5yrs'],
+  split: [2],
   albums: {
     name: ['good news', 'bad news'],
     released: [2022],
   },
-  bio() {
-    `${this.name} is estabished in ${this.nationality} about  ${this.formed} ago with ${this.members} members with ${this.genre} style,the first albums released in ${this.albums.released} with title  ${this.albums.name}.`();
-  },
+  bandInfo = `${this.name} is estabished in ${this.nationality} about 
+   ${this.formed} ago with ${this.members} members with ${this.genre} style,
+   the first albums released in ${this.albums.released} with title  ${this.albums.name}.`
+   ;
 };
 const section = document.querySelector('section');
 // Don't edit the code below here!
