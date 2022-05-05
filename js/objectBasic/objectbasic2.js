62ce10452b694936ac279b026d7ab69ef52cf3eb
debbyblessing <debbyblessing1234@gmail.com>
2022-05-05 11:20:10 +0100

    fix bug

--- js/objectBasic/objectbasic2.js
+++ js/objectBasic/objectbasic2.js
@@ -2,21 +2,23 @@ let bandInfo;
 
 // Put your code here
 const info = {
-  name: ['Miracle band'],
-  nationality: ['Nigeria'],
-  genre: ['R&B'],
-  members: [50],
-  formed: ['5yrs'],
-  split: [2],
+  name: 'Miracle band',
+  nationality: 'Nigeria',
+  genre: 'R&B',
+  members: 50,
+  formed: '5yrs',
+  split: 2,
   albums: {
-    name: ['good news', 'bad news'],
-    released: [2022],
+    firstname: ['good news'],
+    firstreleased: 2022,
+    secondname: ['bad news'],
+    secondreleased: 2021,
   },
 };
 // eslint-disable-next-line prefer-const
 bandInfo = `${info.name} is estabished in ${info.nationality} about 
    ${info.formed} ago with ${info.members} members with ${info.genre} style,
-   the first albums released in ${info.albums.released} with title  ${info.albums.name}.`;
+   the first albums released in ${info.albums.firstreleased} with title  ${info.albums.firstname}.`;
 
 const section = document.querySelector('section');
 // Don't edit the code below here!
