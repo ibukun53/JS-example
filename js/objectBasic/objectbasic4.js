4e3b2a146bdddedce60e2f30acd4610e28e69904
debbyblessing <debbyblessing1234@gmail.com>
2022-05-06 11:56:04 +0100

    fixes

--- js/objectBasic/objectbasic4.js
+++ js/objectBasic/objectbasic4.js
@@ -1,33 +1,15 @@
-/* eslint-disable no-console */
-const cat = {
-  name: 'Bertie',
-  breed: 'Cymric',
-  color: 'white',
-  greeting() {
+function Cat(name, breed, color) {
+  this.name = name;
+  this.breed = breed;
+  this.color = color;
+  this.greeting = function greeting() {
+    // eslint-disable-next-line no-console
     console.log(`Hello, said ${this.name} the ${this.breed}.`);
-  },
-};
+  };
+}
 
-const cat2 = {
-  name: 'Elfie',
-  breed: 'Aphrodite Giant',
-  color: 'ginger',
-  greeting() {
-    console.log(`Hello, said ${this.name} the ${this.breed}.`);
-  },
-};
+const cat = new Cat('Bertie', 'Cymric', 'white');
+const cat2 = new Cat('Elfie', 'Aphrodite Giant', 'ginger');
 
 cat.greeting();
 cat2.greeting();
-
-function createCat(name, breed, color) {
-  const obj = {};
-  obj.name = name;
-  obj.breed = breed;
-  obj.color = color;
-  obj.greeting = function greeting() {
-    console.log(`Hello, said ${this.name} the ${this.breed}.`);
-  };
-  return obj;
-}
-createCat();
