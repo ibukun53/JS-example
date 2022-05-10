c3f8677ec986e8116e7712bb6e12539ebcb8832a
debbyblessing <debbyblessing1234@gmail.com>
2022-05-10 21:12:42 +0100

    fixing bug

--- js/card.js
+++ js/card.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 function Portfoliocard(name, description, language, skills, Framework, link) {
   this.name = name;
   this.description = description;
@@ -6,13 +7,16 @@ function Portfoliocard(name, description, language, skills, Framework, link) {
   this.framework = Framework;
   this.link = link;
   this.createElement = function createElement() {
-    const newCard = document.createElement('h3');
-    const h3 = document.createTextNode(`Hello, ${this.name} is a platform that tells how to ${this.description} using the Language which  are${this.language}, while the Skills are${this.skills},the framework to be use are${this.framework},and ${this.link} to contact us.`);
-    newCard.appendChild(h3);
+    const grid = document.createElement('grid');
+    const textNode = document.createTextNode(`Hello, ${this.name} is a platform that tells how to ${this.description} using the Language which  are${this.language}, while the Skills are${this.skills},the framework to be use are${this.framework},and ${this.link} to contact us.`);
+    /* const element = document.getElementsByClassName('card');
+    element[0].innerHtml = 'hi'; */
+    grid.appendChild(textNode);
+    document.body.appendChild(grid);
   };
 }
 
 const portfolio = new Portfoliocard('Personal portfoilio', 'build a product,feature or website Look through some of my work and experience! If you like what you see and have a project you need coded',
-  ['Javscript,Ruby,Html and Css'], ['Bootstrap,RUby,RSpec,Capybara and Selenium'], [' Codekit, Github,Codepen,Gitlab and Terminal'], 'debbyblessing1234@gmail.com');
+  ['Javscript', 'Ruby', 'Html', 'Css'], ['Bootstrap', 'Ruby', 'RSpec', 'Capybara', 'Selenium'], [' Codekit', 'Github', 'Codepen', 'Gitlab', 'Terminal'], 'debbyblessing1234@gmail.com');
 
-portfolio.createElement('newCard');
+portfolio.createElement();
