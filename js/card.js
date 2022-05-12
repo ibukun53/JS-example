3c5ddf6275252292cd8bc1dbd018c38ecc39c66e
debbyblessing <debbyblessing1234@gmail.com>
2022-05-12 14:05:25 +0100

    fixed bugs

--- js/card.js
+++ js/card.js
@@ -1,14 +1,19 @@
-function Portfoliocard(CardImg, CardHeader, CardTags) {
-  this.CardImg = CardImg;
-  this.CardHeader = CardHeader;
-  this.CardTags = CardTags;
+function Portfoliocard(cardImg, cardHeader, cardTags) {
+  this.cardImg = cardImg;
+  this.cardHeader = cardHeader;
+  this.cardTags = cardTags;
   this.createElement = function createElement() {
+    //  a new element
     const newdiv = document.createElement('div');
-    const element = newdiv.getElementsByClassName('card-img');
-    element[0].innerHTML = `Hello, ${Portfoliocard.CardImg} is a card that describe how a personal portfolio look  like ,the title is ${Portfoliocard.CardHeader} and this languages are used for the creation which are ${Portfoliocard.CardTags}`;
+    // returns of element into html
+    newdiv.innerHTML = `Hello, ${this.cardImg} is a card that describe how a personal portfolio look  like ,the title is ${this.cardHeader} and the languages used for developing are ${this.cardTags}`;
+    return newdiv;
   };
 }
-
+// assigning a new variable  to call the  method
 const portfolio = new Portfoliocard('./image/Screenshot.png', 'Multi-Post Stories Gain+Glory', ['Javscript', 'Ruby on rails', 'Html', 'Css']);
-
-portfolio.createElement();
+// assiging the first existing element to selector
+const newdiv = document.querySelector('.card');
+// assigning a variable
+const port = portfolio.createElement();
+newdiv.appendChild(port);
