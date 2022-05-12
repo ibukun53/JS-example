742efd5547910179aae4893461a26c7b53b32f40
debbyblessing <debbyblessing1234@gmail.com>
2022-05-12 17:46:31 +0100

    fixing variable name

--- js/card.js
+++ js/card.js
@@ -4,16 +4,16 @@ function Portfoliocard(cardImg, cardHeader, cardTags) {
   this.cardTags = cardTags;
   this.createElement = function createElement() {
     //  a new element
-    const newdiv = document.createElement('div');
+    const div = document.createElement('div');
     // returns of element into html
-    newdiv.innerHTML = `Hello, ${this.cardImg} is a card that describe how a personal portfolio look  like ,the title is ${this.cardHeader} and the languages used for developing are ${this.cardTags}`;
-    return newdiv;
+    div.innerHTML = `Hello, ${this.cardImg} is a card that describe how a personal portfolio look  like ,the title is ${this.cardHeader} and the languages used for developing are ${this.cardTags}`;
+    return div;
   };
 }
 // assigning a new variable  to call the  method
 const portfolio = new Portfoliocard('./image/Screenshot.png', 'Multi-Post Stories Gain+Glory', ['Javscript', 'Ruby on rails', 'Html', 'Css']);
 // assiging the first existing element to selector
-const newdiv = document.querySelector('.card');
+const div = document.querySelector('.card');
 // assigning a variable
 const port = portfolio.createElement();
-newdiv.appendChild(port);
+div.appendChild(port);
