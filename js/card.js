5ed74744562d2b624982805ecbde35e9c3c59dbf
debbyblessing <debbyblessing1234@gmail.com>
2022-05-16 13:30:35 +0100

    working on new function

--- js/card.js
+++ js/card.js
@@ -11,12 +11,9 @@ function PortfolioCard(cardImg, cardHeader, cardTags) {
               <div class="card">
                 <img class="card-img" src="${this.cardImg}" alt="page" />
                 <h3 class="card-header">${this.cardHeader}</h3>
-                <ul class="card-content margin-left-right-auto">
-                  <li class="content-tags">${this.cardTags}</li>
-                  <li class="content-tags">css</li>
-                  <li class="content-tags">Javascript</li>
-                  <li class="content-tags">html</li>
-                </ul>
+                <ul class="card-content margin-left-right-auto">${this.contentTags}
+                  <li class="content-tags">Javascriptli</li>
+                 </ul>
                 <div class="content-btn">
                 <a href="#" aria-label="button">
                 <button-style onclick ="myFunction()" class="see-projects">See Project </button-style>
@@ -28,8 +25,14 @@ function PortfolioCard(cardImg, cardHeader, cardTags) {
     return div;
   };
 }
+const contentTags = ['html', 'css', 'javascript'];
+const newlang = contentTags => contentTags.map(e => `<li class="content-tags">${e}</li>`);
+
+// eslint-disable-next-line no-console
+console.log(newlang(contentTags));
+
 // assigning a new variable  to call the  method
-const portfolioCard = new PortfolioCard('./image/Snapshoot Portfolio.png', 'Multi-Post Stories Gain+Glory', ['Javscript', 'Ruby on rails', 'Html', 'Css']);
+const portfolioCard = new PortfolioCard('./image/Snapshoot Portfolio.png', 'Multi-Post Stories Gain+Glory', ['Ruby on rails', 'Html', 'Css']);
 // assiging the first existing element to selector
 const div = document.querySelector('.card');
 // assigning a variable
@@ -43,5 +46,5 @@ document.querySelector('.card-img').style.cssText = 'display: block; width: 100%
 document.querySelector('.card-header').style.cssText = 'display: block; font-size: 140%; width: 70%; height: 8%; font-weight: 600; color: rgb(58, 74, 66); margin: auto; text-align: center; justify-content: center; font-style: normal;lineHeight: 32px';
 document.querySelector('.card-content').style.cssText = 'display: flex;width: 96%;flex-wrap: wrap;align-content: center;justify-content: space-between;align-items: center;text-align: center;margin-top: 10%;';
 document.querySelector('.content-tags').style.cssText = 'border-radius: 4px; margin-top: 12px; margin-bottom: 10px; background: rgba(235, 240, 238, 1);padding: 2%;font-size: 12px;font-style: normal;font-weight: 600;letter-spacing: 0.03em;color: black';
-document.querySelector('.content-btn').style.cssText = 'border-radius: 4px; padding: 2%; margin: 10% 42% 5%;; background:  #805500; align-items: center; filter: contrast(150%)';
+document.querySelector('.content-btn').style.cssText = 'border-radius: 4px; padding: 2%; margin: 10% 33% 5%;; background:  #805500; align-items: center; filter: contrast(150%)';
 document.querySelector('.see-projects').style.cssText = '; text-decoration: none; text-align: center;  margin: auto;;border: 0; color: rgb(255, 255, 255); background:  #805500;';
