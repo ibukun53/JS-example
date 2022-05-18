3853e0deefcb7f61a4a2aa95fdd54ac708ab1c9a
debbyblessing <debbyblessing1234@gmail.com>
2022-05-18 16:32:02 +0100

    fixed bug

--- js/card.js
+++ js/card.js
@@ -3,9 +3,9 @@ function PortfolioCard(cardImg, cardHeader, cardTags) {
   this.cardHeader = cardHeader;
   this.cardTags = cardTags;
   this.createElement = function createElement() {
-    const newlang = cardTags => cardTags.map(e => `<li class="content-tags">${e}</li>`);
-    // eslint-disable-next-line no-console
-    console.log(newlang.join(''));
+    const tags = cardTags => cardTags.map(e => `<li class="content-tags">${e}</li>`).join('');
+    tags(cardTags);
+    const card = tags(cardTags);
     const div = document.createElement('div');
     // returns of element into html
     div.innerHTML = ` <div class="grid">
@@ -13,9 +13,7 @@ function PortfolioCard(cardImg, cardHeader, cardTags) {
               <div class="card">
                 <img class="card-img" src="${this.cardImg}" alt="page" />
                 <h3 class="card-header">${this.cardHeader}</h3>
-                <ul class="card-content margin-left-right-auto>
-                  <li class="content-tags">Javascriptli</li>
-                 </ul>
+                <ul class="card-content margin-left-right-auto">${card} </ul>
                 <div class="content-btn">
                 <a href="#" aria-label="button">
                 <button-style onclick ="myFunction()" class="see-projects">See Project </button-style>
@@ -30,18 +28,8 @@ function PortfolioCard(cardImg, cardHeader, cardTags) {
 
 // assigning a new variable  to call the  method
 const portfolioCard = new PortfolioCard('./image/Snapshoot Portfolio.png', 'Multi-Post Stories Gain+Glory', ['Javascript', 'Ruby on rails', 'Html', 'Css']);
-// assiging the first existing element to selector
+// assiging the  first existing element to selector
 const div = document.querySelector('.card');
 // assigning a variable
 const portfolio = portfolioCard.createElement();
 div.appendChild(portfolio);
-
-document.querySelector('.grid').style.cssText = 'width: auto; height: auto; alignItems: start; marginTop: 5%;';
-document.querySelector('.grid-item').style.cssText = 'display: inline- block; border: 6px solid #e5e5e5; background-color: rgba(255, 255, 255, 1); width: 60%; height: 70%; margin: auto; border-radius: 25px; overflow: hidden; cursor: pointer;transition: 0.2s;';
-document.querySelector('.card').style.cssText = 'box-sizing: borderBox; border: 5px; width: 100%; height: 40%; margin-bottom: 5px;';
-document.querySelector('.card-img').style.cssText = 'display: block; width: 100%; height: 30%;';
-document.querySelector('.card-header').style.cssText = 'display: block; font-size: 140%; width: 70%; height: 8%; font-weight: 600; color: rgb(58, 74, 66); margin: auto; text-align: center; justify-content: center; font-style: normal;lineHeight: 32px';
-document.querySelector('.card-content').style.cssText = 'display: flex;width: 96%;flex-wrap: wrap;align-content: center;justify-content: space-between;align-items: center;text-align: center;margin-top: 10%;';
-document.querySelector('.content-tags').style.cssText = 'border-radius: 4px; margin-top: 12px; margin-bottom: 10px; background: rgba(235, 240, 238, 1);padding: 2%;font-size: 12px;font-style: normal;font-weight: 600;letter-spacing: 0.03em;color: black';
-document.querySelector('.content-btn').style.cssText = 'border-radius: 4px; padding: 2%; margin: 10% 33% 5%;; background:  #805500; align-items: center; filter: contrast(150%)';
-document.querySelector('.see-projects').style.cssText = '; text-decoration: none; text-align: center;  margin: auto;;border: 0; color: rgb(255, 255, 255); background:  #805500;';
